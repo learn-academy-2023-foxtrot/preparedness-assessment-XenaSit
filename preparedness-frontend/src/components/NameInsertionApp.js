afef7106a864cea1cfa5ab936063df91b4ec7519
XenaSit <sitnikovaxena@gmail.com>
2023-10-14 13:47:29 -0700

    got a little trouble with input being 50 percent

--- preparedness-frontend/src/components/NameInsertionApp.js
+++ preparedness-frontend/src/components/NameInsertionApp.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Input, Label } from "reactstrap"
+import "../App.css"
+// import Alert from '@mui/material/Alert';
 
 function NameInsertionApp() {
   const [name, setName] = useState('');
@@ -10,24 +12,33 @@ function NameInsertionApp() {
     setName(enteredName);
     setText(`${enteredName}!`);
   };
+  
+  const onClickReset = () => {
+    setText('');
+    setName('');
+  };
 
   function showAlert() {
-    alert(`Hello ${name} Swab scuttle grog blossom main sheet pirate lass yardarm broadside doubloon avast pink ho wench chandler warp. League yard cackle fruit code of conduct quarter loaded to the gunwalls American Main lass Buccaneer Barbary Coast stern gangplank Sail ho hempen halter brigantine. Cutlass yardarm jack weigh anchor keel code of conduct splice the main brace ye schooner prow belay gally long clothes brig port.`);
+    alert
+    (    
+`Hello there ${name}!
+
+Swab scuttle grog blossom main sheet pirate lass yardarm broadside doubloon avast pink ho wench chandler warp. League yard cackle fruit code of conduct quarter loaded to the gunwalls American Main lass Buccaneer Barbary Coast stern gangplank Sail ho hempen halter brigantine. Cutlass yardarm jack weigh anchor keel code of conduct splice the main brace ye schooner prow belay gally long clothes brig port`);
   }
 
   return (
     <div>
       <p>Hello {text}</p>
+
       <Label for="name">Enter your name</Label>
-      <Input type="text"
-      placeholder="Enter your name"
-      onChange={handleInputChange}/>
       
-      <Button color="primary" onClick={showAlert}>
-        Click Me
-      </Button>
-      
-      <Button>Reset</Button>
+      <Input type="text" placeholder="Enter your name" onChange={handleInputChange}/>
+
+      <div className="button-container">
+        <Button color="primary" onClick={showAlert} >Click Me</Button>
+        <Button color="primary" value="reset" onClick={onClickReset}>Reset</Button>
+      </div>
+
     </div>
   );
 }
